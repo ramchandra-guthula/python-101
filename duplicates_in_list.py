5a747e9398eff6c40a8f68787bb91e31dd8fd098
Ramchandra Guthula <64400940+ramchandra-guthula@users.noreply.github.com>
2025-10-02 13:31:46 -0500

    Update duplicates_in_list.py

--- duplicates_in_list.py
+++ duplicates_in_list.py
@@ -4,3 +4,8 @@ for i in range(len(sample_list)): # Looping through index
     for p in range(i+1, len(sample_list)):
         if sample_list[i] == sample_list[p]: # Taking first element in sample_list and compare with all the elements
             print(sample_list[i])
+
+# Updated version
+my_list = [1, 2, 2, 3, 4, 4, 5]
+unique_list = list(set(my_list))
+print(unique_list)
